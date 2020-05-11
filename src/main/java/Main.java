6bf00ebc2cae8eba796e838a68ad7b19cdb37aec
Cebanu Dumitrita <dumitrita.cebanu.99@gmail.com>
2020-05-12 01:14:16 +0300

    SPOR-30 #Homepage interface-done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,25 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.util.Objects;
+
 
 public class Main extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("fxmlFiles/LoginScreen.fxml")));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, 630, 400));
+        primaryStage.show();
     }
 
-    @Override
-    public void start(Stage primaryStage) {
 
+    public static void main(String[] args) {
+
+        launch(args);
     }
 }
