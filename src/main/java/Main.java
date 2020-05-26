ebe2f7ac982ee1786b2d28df05fe4cfbb03b6750
Condurari Radu <brockbraun@gmail.com>
2020-05-26 14:52:16 +0300

    SPOR-22 Am creat un fisier Json unde se voi adauga ca ADMIN clientii

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("fxmlFiles/LoginScreen.fxml")));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxmlFiles/LoginScreen.fxml"));
         primaryStage.setTitle("Login");
         primaryStage.setScene(new Scene(root, 630, 400));
         primaryStage.show();
